33a9303904b9812cb4b8dd26b8dc0f0c51ed4cab
Soumyajagupta07 <guptasoumyaja@gmail.com>
2023-06-24 13:48:11 +0530

    committed

--- home.js
+++ home.js
@@ -1,3 +1,16 @@
-function onclick(){
-    
+function clicked(){
+    location.href = "home.html"
 }
+let audioElement = new Audio("Music/Manike Mage Hithe.mp3")
+let bottom_play = document.getElementById("bottom_play");
+bottom_play.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime == 0)
+        audioElement.play();
+    let pause = document.createElement('img');
+    pause.src = "pause.jpeg";
+    let pause_img = document.getElementById('pause_img');
+    pause_img.appendChild(pause);
+    bottom_play.style.display = 'none';
+    pause.style.height = '20px';
+    pause.style.width = '20px';
+})
